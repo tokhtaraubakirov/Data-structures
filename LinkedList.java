d893e46a1f355b8f9807981a2b081029defc6085
tokhtaraubakirov <student.aubakirov@dku.kz>
2023-01-11 22:45:36 +0600

    first commit

---
+++ LinkedList.java
@@ -0,0 +1,29 @@
+//Single LinkedList implementation;
+public class LinkedList {
+    Node head;
+
+    public static class Node {
+        int data; //defining our data;
+        Node next; //defining next pointer to the next NODE;
+
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static void main(String[]args){
+        LinkedList list = new LinkedList();
+
+        Node node3 = new Node(10);
+        node3.next = null;
+
+        Node node2 = new Node(5);
+        node2.next = node3;
+
+        Node node1 = new Node(1);
+        node1.next = node2;
+
+        list.head = node1;
+    }
+}
